5e8591e8635ce14ecef8912d3682f279e2d67b93
Chanhor-debug <ramchanhorsiro@gmail.com>
2024-11-06 21:39:16 +0530

    Add files via upload

---
+++ Q7.vb
@@ -0,0 +1,48 @@
+
+
+Module Program
+    Public Interface IAnimal
+        Sub Speak()
+    End Interface
+
+
+    Public Class Dog
+        Implements IAnimal
+
+        Public Sub Speak() Implements IAnimal.Speak
+            Console.WriteLine(" Woof! Woof! Woof! Woof!")
+        End Sub
+    End Class
+
+    Public Interface IFlyable
+        Sub Fly()
+    End Interface
+
+    Public Interface ISwimmable
+        Sub Swim()
+    End Interface
+
+    Public Class Duck
+        Implements IFlyable
+        Implements ISwimmable
+
+        Public Sub Fly() Implements IFlyable.Fly
+            Console.WriteLine("The bird is flying!")
+        End Sub
+
+        Public Sub Swim() Implements ISwimmable.Swim
+            Console.WriteLine("The duck is swimming!")
+        End Sub
+    End Class
+
+    Sub Main()
+        ' Using the single interface
+        Dim myDog As IAnimal = New Dog()
+        myDog.Speak()
+
+        ' Using the multi-interface
+        Dim myDuck As Duck = New Duck()
+        myDuck.Fly()
+        myDuck.Swim()
+    End Sub
+End Module
